fix one no gradient issue

decoder.py
@@ -164,8 +164,6 @@ class DecoderLayer(nn.Module):
     # self.query [1(extends to bs), num_query, embed_dims]
     self.query             = self.NNP_query_origin + self.NNP_query_pos
     self.NN_ref_points     = nn.Linear(self.embed_dims, 3, device=device)
-    # self.reference_points [1(extends to bs), num_query, 3]
-    self.reference_points  = self.NN_ref_points(self.NNP_query_pos)
     self.NN_fullAttn       = FullAttention(full_dropout,full_embed_dims,full_num_heads,full_num_levels,full_num_points,device)
     self.NN_custAttn       = CustomAttention(custom_dropout,custom_embed_dims,custom_num_heads,custom_num_levels,custom_num_points,device)
     self.NN_addNorm1       = AddAndNormLayer(None,embed_dims,device=device)
@@ -185,6 +183,8 @@ class DecoderLayer(nn.Module):
       init_references_points    (Tensor             [bs, num_query, 3])
       listed_deocerlayer_feat   (list of Tensor [[bs, embed_dims, H, W], ... ])
     """
+    # self.reference_points [1(extends to bs), num_query, 3]
+    self.reference_points  = self.NN_ref_points(self.NNP_query_pos)
     bs, num_key, emded_dims = key.shape
     # self.query [1,  num_query, embed_dims]
     # ---->query [bs, num_query, embed_dims]

encoder.py
@@ -143,8 +143,7 @@ class EncoderLayer(nn.Module):
     self.NN_projQ          = nn.Linear(query_C,temp_embed_dims).to(device)
     # self.NNP_query_pos [1(extends to bs), num_query, embed_dims]
     self.NNP_query_pos     = nn.Parameter(torch.ones(1,query_H*query_W,temp_embed_dims,device=device)*0.95)
-    # self.query [1(extends to bs), num_query, embed_dims]
-    self.query             = self.NN_projQ(self.NNP_query_origin) + self.NNP_query_pos
+    
     # self.temp_spatial_shapes [level(fixed to 1 here), 2]
     self.temp_spatial_shapes = torch.Tensor([[query_H, query_W]]).to(device)
     self.NN_tempAttn    = TemporalSelfAttention(temp_num_sequences,temp_dropout,temp_embed_dims,temp_num_heads,temp_num_levels,temp_num_points,device)
@@ -153,8 +152,7 @@ class EncoderLayer(nn.Module):
     self.NN_addNorm2    = AddAndNormLayer(None,embed_dims,device=device)
     self.NN_addNorm3    = AddAndNormLayer(None,embed_dims,device=device)
     self.NN_ffn         = nn.Linear(embed_dims,embed_dims,device=device)
-    self.temp_key_hist     = [self.query for _ in range(temp_num_sequences)]
-    self.temp_value_hist   = [self.query for _ in range(temp_num_sequences)]
+    
   def forward(self,spat_key,spat_value,spat_spatial_shapes=None,spat_lidar2img_trans=None):
     """
     Args:
@@ -165,6 +163,10 @@ class EncoderLayer(nn.Module):
     Returns:
       stacked_currentBEV          (Tensor [num_layers, bs, num_query, emded_dims])
     """
+    # self.query [1(extends to bs), num_query, embed_dims]
+    self.query             = self.NN_projQ(self.NNP_query_origin) + self.NNP_query_pos
+    self.temp_key_hist     = [self.query for _ in range(self.temp_num_sequences)]
+    self.temp_value_hist   = [self.query for _ in range(self.temp_num_sequences)]
     _,bs,_,_ = spat_key.shape
     ref_3d, ref_2d = self.cal_reference_points(self.query_Z,bs)
     spat_reference_points_cam, spat_bev_mask = self.sample_points(ref_3d, spat_lidar2img_trans)